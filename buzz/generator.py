7a224910f13382fe986fb4d3d5a60940ea84f0f6
singhpawan <pawandeep.me@gmail.com>
2018-12-13 11:13:54 -0500

    modified generator.py

--- buzz/generator.py
+++ buzz/generator.py
@@ -14,6 +14,7 @@ def sample(l, n = 1):
     return result
 
 def generate_buzz():
+    print "Generating buzz .... "
     buzz_terms = sample(buzz, 2)
     phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
         sample(verbs), buzz_terms[1]])
